9e4653f2b043356a58b22ab0f70fd05a7b489da4
rn286 <rn286@njit.edu>
2017-11-19 18:24:07 -0500

    model4

--- model.php
+++ model.php
@@ -23,7 +23,7 @@ abstract class model {
 	}
 
     private function insert() {
-		$sql = "INSERT INTO (" . $this->$columnString . ") VALUES (" . $this->$valueString . ")</br>";
+		$sql = "INSERT INTO " . $this->tableName() . "(" . $this->$columnString . ") VALUES (" . $this->$valueString . ")";
         return $sql;
         
 		echo 'I just inserted record' . $this->id;
